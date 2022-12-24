7067ef1e10f8c323ea47b5db952b165c1fdf79ea
Souvick Chakraborty <114563269+githubsouvick7@users.noreply.github.com>
2022-12-24 17:24:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 const App = () => {
+  const [value, setValue] = useState('square');
+  const [shapeList, setShapeList] = useState([]);
+  function handleChange() {
+    setShapeList([...shapeList, value])
+  }
 
   return (
     <div id="main">
       <div id="shape-creator">
-        
+        <select onChange ={(e) => { setValue(e.target.value)}}>
+          <option value={square}>Square</option>
+          <option value={circle}>Circle</option>
+        </select>
+        <button onClick={haldleChange}>Add Shape</button>
 
       </div>
       <div id="shapes-holder">
+        {shapeList.map((ele, idx) => ele === 'square' ? <div className='square'>{idx}</div> : <div className='circle'>{idx}</div>)}
       </div>
     </div>
   )
